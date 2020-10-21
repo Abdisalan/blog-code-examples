375a93eafea0c69aa581ac4cb218a48932e4d57e
Abdisalan Mohamud <abdisalan.mohamud@outlook.com>
2020-10-20 21:59:38 -0600

    slight adjustments

--- http-video-stream/index.js
+++ http-video-stream/index.js
@@ -14,21 +14,22 @@ app.get("/video", function (req, res) {
     res.status(400).send("Requires Range header");
   }
 
-  // get video stats
+  // get video stats (about 61MB)
   const videoPath = "bigbuck.mp4";
   const videoSize = fs.statSync("bigbuck.mp4").size;
 
   // Parse Range
+  // Example: "bytes=32324-"
   const CHUNK_SIZE = 10 ** 6; // 1MB
   const start = Number(range.replace(/\D/g, ""));
   const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
 
   // Create headers
-  const length = end - start + 1;
+  const contentLength = end - start + 1;
   const headers = {
     "Content-Range": `bytes ${start}-${end}/${videoSize}`,
     "Accept-Ranges": "bytes",
-    "Content-Length": length,
+    "Content-Length": contentLength,
     "Content-Type": "video/mp4",
   };
 
@@ -37,6 +38,7 @@ app.get("/video", function (req, res) {
 
   // create video read stream for this particular chunk
   const videoStream = fs.createReadStream(videoPath, { start, end });
+
   // Stream the video chunk to the client
   videoStream.pipe(res);
 });
