fc827aff10a308c04fff73410dcf9734319a0c03
Abdisalan Mohamud <abdi.mohamud94@gmail.com>
2020-11-22 17:01:13 -0700

    slight simplified stream code

--- mongo-http-video/index.js
+++ mongo-http-video/index.js
@@ -15,40 +15,40 @@ app.get('/init-video', function (req, res) {
       res.json(error);
       return;
     }
-
     const db = client.db('videos');
-    var bucket = new mongodb.GridFSBucket(db);
-    fs.createReadStream('./bigbuck.mp4').pipe(bucket.openUploadStream('bigbuck.mp4'));
-    res.json("Done...");
+    const bucket = new mongodb.GridFSBucket(db);
+    const videoUploadStream = bucket.openUploadStream('bigbuck');
+    const videoReadStream = fs.createReadStream('./bigbuck.mp4');
+    videoReadStream.pipe(videoUploadStream);
+    res.status(200).send("Done...");
   });
 });
 
 app.get("/mongo-video", function (req, res) {
   mongodb.MongoClient.connect(url, function (error, client) {
     if (error) {
-      res.json(error);
+      res.status(500).json(error);
       return;
     }
 
-    // Ensure there is a range given for the video
     const range = req.headers.range;
     if (!range) {
       res.status(400).send("Requires Range header");
     }
 
     const db = client.db('videos');
+    // GridFS Collection
     db.collection('fs.files').findOne({}, (err, video) => {
       if (!video) {
         res.status(404).send("No video uploaded!");
         return;
       }
 
-      // Find video size and range
+      // Create response headers
       const videoSize = video.length;
       const start = Number(range.replace(/\D/g, ""));
       const end = videoSize - 1;
 
-      // Create headers
       const contentLength = end - start + 1;
       const headers = {
         "Content-Range": `bytes ${start}-${end}/${videoSize}`,
@@ -60,14 +60,13 @@ app.get("/mongo-video", function (req, res) {
       // HTTP Status 206 for Partial Content
       res.writeHead(206, headers);
 
-      // create video read stream for this particular chunk
       const bucket = new mongodb.GridFSBucket(db);
-      const downStream = bucket.openDownloadStreamByName('bigbuck.mp4', {
+      const downloadStream = bucket.openDownloadStreamByName('bigbuck', {
         start
       });
 
       // Finally pipe video to response
-      downStream.pipe(res);
+      downloadStream.pipe(res);
     });
   });
 });
